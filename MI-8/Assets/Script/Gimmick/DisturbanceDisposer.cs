c8beab1d65e500851263fbb010f25e5c7d68297d
kangyh <kangyh@gmail.com>
2018-01-27 12:58:03 +0900

    妨害物のディスポーザーにタップされてからイベント起こすまでの時間を設定できるようにした

--- MI-8/Assets/Script/Gimmick/DisturbanceDisposer.cs
+++ MI-8/Assets/Script/Gimmick/DisturbanceDisposer.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class DisturbanceDisposer : MonoBehaviour
 {
+    [SerializeField]
+    private float FireInterval = 0f;
     private readonly ISubject<DisturbanceDisposer> onTapped = new Subject<DisturbanceDisposer>();
     public IObservable<DisturbanceDisposer> OnTappedAsObservable()
     {
@@ -16,6 +18,7 @@ public class DisturbanceDisposer : MonoBehaviour
     {
         ObservableInput.Instance.OnTappedAsObservable()
             .Where(tappedObj => tappedObj == this.gameObject)
+            .Delay(TimeSpan.FromSeconds(FireInterval))
             .Subscribe(_ => onTapped.OnNext(this))
             .AddTo(this);
     }
