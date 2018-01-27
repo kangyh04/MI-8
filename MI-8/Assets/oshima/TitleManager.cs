bbd50c4c285e0c95f58b7a02686bd771cc9e763a
大島有貴 <yuki.nasu.0225@docomo.ne.jp>
2018-01-27 09:50:36 +0900

    タイトル画面とスタッフ画面、ステージ１とスタッフ画面からタイトルに戻るボタンを作りました。

---
+++ MI-8/Assets/oshima/TitleManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void PushStartButton(){
+		SceneManager.LoadScene ("Stage1");
+	}
+	public void PushStaffButton(){
+		SceneManager.LoadScene ("Staff");
+	}
+}
