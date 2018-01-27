412fced4e5675321f8ba7b48b114475c9185af14
G-TUNENEXTGEAR-\takoi <tako.ishihata@gmail.com>
2018-01-27 18:13:36 +0900

    add scenemanager.

--- MI-8/Assets/Script/RidedPoweredSuite/RidedPoweredSuite.cs
+++ MI-8/Assets/Script/RidedPoweredSuite/RidedPoweredSuite.cs
@@ -1,27 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RidedPoweredSuite : MonoBehaviour
 {
     // move speed
     public float PwSpeed = 5.0f;
     public float PwVspeed = 10.0f;
+    public float PwCounterMax = 8.0f;
+    public bool IsGoaled = false;
 
     // private values
-    bool isGoaled = false;
-
+    float EndCounter = 0.0f;
 
     // Use this for initialization
     void Start()
     {
-        isGoaled = false;
+        IsGoaled = false;
+        EndCounter = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isGoaled == false)
+        if (IsGoaled == false)
         {
             // move horizontal
             Vector3 pos = transform.position;
@@ -34,6 +37,13 @@ public class RidedPoweredSuite : MonoBehaviour
             Vector3 pos = transform.position;
             pos.y += PwVspeed * Time.deltaTime;
             transform.position = pos;
+
+            // counter
+            EndCounter += Time.deltaTime;
+            if(EndCounter > PwCounterMax )
+            {
+                //SceneManager.LoadScene("GameTitle");
+            }
         }
     }
 
@@ -43,7 +53,7 @@ public class RidedPoweredSuite : MonoBehaviour
         // Hanger stage clear
         if (collision.tag == "Ladders")
         {
-            isGoaled = true;
+            IsGoaled = true;
             Debug.Log("GAME CLEAR!!");
         }
     }
